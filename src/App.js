3a0b22ab07880d03d58d644c50ef13c829357aa4
Sebastian Tostado <stostad@umich.edu>
2022-08-04 09:25:28 -0500

    Test for pipeline and deployment

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2>This is a pipline and deploy test!!</h2>
+        <h3> Hello World</h3>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
